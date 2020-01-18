8e43662430798bf047f9734a810d6f7dc4c12bff
sankalp <sjain4688@gmail.com>
2020-01-19 03:39:02 +0530

    carousel part1 => only images are shifting

--- chase/src/components/carousel/Carousel.js
+++ chase/src/components/carousel/Carousel.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
-import ImageSlide from './ImageSlide';
-import Arrow from './Arrow';
+import React, { Component } from "react";
+import ImageSlide from "./ImageSlide";
+import Arrow from "./Arrow";
+
+const imgUrls = ["carousel-img/1.jpg", "carousel-img/2.jpg"];
 class Carousel extends Component {
   constructor(props) {
     super(props);
@@ -8,6 +10,27 @@ class Carousel extends Component {
       currentImageIndex: 0
     };
   }
+
+  previousSlide = () => {
+    const lastIndex = imgUrls.length - 1;
+    const { currentImageIndex } = this.state;
+    const shouldResetImageIndex = currentImageIndex === 0;
+    const index = shouldResetImageIndex ? lastIndex : currentImageIndex - 1;
+
+    this.setState({
+      currentImageIndex: index
+    });
+  };
+
+  nextSlide = () => {
+    const lastIndex = imgUrls.length - 1;
+    const { currentImageIndex } = this.state;
+    const shouldResetImageIndex = currentImageIndex === lastIndex;
+    const index = shouldResetImageIndex ? 0 : currentImageIndex + 1;
+    this.setState({
+      currentImageIndex: index
+    });
+  };
 
   render() {
     return (
@@ -17,7 +40,7 @@ class Carousel extends Component {
           clickFunction={this.previousSlide}
           glyph='&#9664;'
         />
-        <ImageSlide url={'header-back.jpg'} />
+        <ImageSlide url={imgUrls[this.state.currentImageIndex]} />
         <Arrow
           direction='right'
           clickFunction={this.nextSlide}
