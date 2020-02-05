0dd2981a354b5804ce5e053f49e2fccd4c8bc032
sankalp <sjain4688@gmail.com>
2020-02-05 18:57:49 +0530

    mosaic start

--- chase/src/components/mosaic/MosaicContainer.js
+++ chase/src/components/mosaic/MosaicContainer.js
@@ -3,8 +3,12 @@ import MosaicTile from './MosaicTile';
 export default class MosaicContainer extends Component {
   render() {
     return (
-      <div>
-        <MosaicTile />
+      <div style={{ textAlign: 'center', margin: 'auto' }}>
+        <div className='row' style={{ width: '70%' }}>
+          <MosaicTile />
+          <MosaicTile />
+          <MosaicTile />
+        </div>
       </div>
     );
   }
--- chase/src/components/mosaic/MosaicTile.js
+++ chase/src/components/mosaic/MosaicTile.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 
 export default class MosaicTile extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className='mosaic--tile col-md-4 border' style={{ height: '10%' }}>
+        fgdfgdfgdf
+      </div>
+    );
   }
 }
