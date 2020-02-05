7df50b3be4be8d189c7fa38508ec124ea16c7494
sankalp <sjain4688@gmail.com>
2020-02-05 15:17:47 +0530

    carousel working --2 needs more work

--- chase/src/components/carousel/Carousel.js
+++ chase/src/components/carousel/Carousel.js
@@ -22,62 +22,149 @@ const imgUrls = [
   { index: 6, icon: Briefcase, subtitle: 'Chase for Business' },
   { index: 7, icon: PrivateClient, subtitle: 'Chase Private Client' },
   { index: 8, icon: Invest, subtitle: 'Invest' }
+  // { index: 0, icon: Tachometer, subtitle: 'Free Credit Score' }
 ];
 class Carousel extends Component {
   constructor(props) {
     super(props);
-    this.state = { cards: imgUrls };
+    this.state = {
+      cards: imgUrls,
+      card: imgUrls[0],
+      transition: 0
+      // first: 0,
+      // second: 1,
+      // third: 2,
+      // fourth: 3,
+      // current: 4
+    };
   }
 
-  componentDidUpdate() {
-    $('#myCarousel').on('slide.bs.carousel', function(e) {
-      var $e = $(e.relatedTarget);
-      var idx = $e.index();
-      var itemsPerSlide = 3;
-      var totalItems = $('.carousel-item').length;
-
-      if (idx >= totalItems - (itemsPerSlide - 1)) {
-        var it = itemsPerSlide - (totalItems - idx);
-        for (var i = 0; i < it; i++) {
-          // append slides to end
-          if (e.direction == 'left') {
-            $('.carousel-item')
-              .eq(i)
-              .appendTo('.carousel-inner');
-          } else {
-            $('.carousel-item')
-              .eq(0)
-              .appendTo($(this).find('.carousel-inner'));
-          }
-        }
-      }
-    });
-  }
+  componentDidUpdate() {}
   //working
-  previousSlide = () => {};
-
-  nextSlide = () => {};
+  previousSlide = () => {
+    console.log(this.state.card);
+    const oldIndex = this.state.card.index;
+    if (oldIndex != 0) {
+      const newIndex = oldIndex - 1;
+      this.setState({
+        card: this.state.cards[newIndex],
+        //cards: cards
+        transition: this.state.transition - 10 - newIndex
+      });
+    } else {
+      this.setState({
+        card: this.state.cards[4]
+      });
+    }
+  };
+  nextSlide = () => {
+    const oldIndex = this.state.card.index;
+    if (oldIndex != this.state.cards.length - 5) {
+      // let cards = this.state.cards;
+      // cards.push(cards[this.state.cards.length - oldIndex - 1]);
+      // // cards.shift();
+      // cards.forEach(function(part, i) {
+      //   this[i].index = i;
+      // }, cards);
+      // console.log(cards);
+      const newIndex = oldIndex + 1;
+      this.setState({
+        card: this.state.cards[newIndex],
+        //cards: cards
+        transition: this.state.transition + 10 + newIndex
+      });
+    } else {
+      this.setState({
+        card: this.state.cards[0]
+      });
+    }
+  };
+  // nextSlide = () => {
+  //   const { current } = this.state;
+  //   this.setState({
+  //     fourth: current,
+  //     third: this.state.fourth,
+  //     second: this.state.third,
+  //     first: this.state.second
+  //   });
+  //   this.setState({ current: (current + 1) % this.state.cards.length });
+  // };
 
   render() {
+    const { card, cards, current, first, second, third, fourth } = this.state;
     return (
       <div className='container-fluid'>
         <span className='carousel__heading'>Choose what's right for you</span>
-        <div style={{}}>
+        <div style={{ position: 'relative', display: 'flex' }}>
+          <div className='slide_arrow--left'>
+            <Arrow
+              direction='left'
+              clickFunction={this.previousSlide}
+              glyph={<i class='fa fa-angle-left'></i>}
+            />
+          </div>
           <div
-            className=''
             style={{
-              backgroundColor: 'yellow',
+              // backgroundColor: 'yellow',
               display: 'flex',
-              width: '70%'
+              position: 'relative',
+              width: '70%',
+              overflow: 'hidden'
             }}
           >
-            {this.state.cards.map(card => (
-              <ImageSlide
-                id={`slick-${card.index}-active`}
-                Icon={card.icon}
-                subtitle={card.subtitle}
-              />
-            ))}
+            <div
+              className=''
+              style={{
+                // backgroundColor: 'yellow',
+                display: 'flex',
+                position: 'absolute',
+                transition:
+                  'transform 300ms cubic-bezier(0.455, 0.03, 0.515, 0.955),visibility 0s, opacity 0.5s linear',
+                transform: `translate3d(-${card.index *
+                  (100 / cards.length)}%, 0px, 0px)`
+              }}
+            >
+              {this.state.cards.map((card, i) => (
+                <ImageSlide
+                  transition={this.state.transition}
+                  id={`slick-${i}-active`}
+                  Icon={card.icon}
+                  subtitle={card.subtitle}
+                />
+              ))}
+              {/* <ImageSlide
+              id={`slick-${this.state.cards[first].index}`}
+              Icon={cards[first].icon}
+              subtitle={cards[first].subtitle}
+            />
+            <ImageSlide
+              id={`slick-${cards[second].index}`}
+              Icon={cards[second].icon}
+              subtitle={cards[second].subtitle}
+            />
+            <ImageSlide
+              id={`slick-${cards[third].index}`}
+              Icon={cards[third].icon}
+              subtitle={cards[third].subtitle}
+            />
+            <ImageSlide
+              id={`slick-${cards[fourth].index}`}
+              Icon={cards[fourth].icon}
+              subtitle={cards[fourth].subtitle}
+            />
+            <ImageSlide
+              id={`slick-${cards[current].index}`}
+              Icon={cards[current].icon}
+              subtitle={cards[current].subtitle}
+            /> */}
+            </div>
+          </div>
+          <div className='slide_arrow--right'>
+            <Arrow
+              direction='right'
+              clickFunction={this.nextSlide}
+              glyph={<i class='fa fa-angle-right'></i>}
+            />
           </div>
         </div>
       </div>
--- chase/src/components/carousel/ImageSlide.js
+++ chase/src/components/carousel/ImageSlide.js
@@ -1,25 +1,36 @@
 import React from 'react';
-const ImageSlide = ({ Icon, subtitle, hide, id }) => (
-  <div id={id}>
-    <p
-      className={hide}
-      className='carousel_item'
-      // className='border'
+const ImageSlide = ({ Icon, subtitle, hide, id, transition }) => (
+  <div>
+    <div
+      id={id}
+      style={
+        {
+          // transition:
+          //   'transform 300ms cubic-bezier(0.455, 0.03, 0.515, 0.955),visibility 0s, opacity 0.5s linear',
+          // transform: `translate3d(-${transition}%, 0px, 0px)`
+        }
+      }
     >
-      <img id='slider' src={Icon} />
       <p
-        className={``}
-        style={{
-          color: '#126BC5',
-          fontWeight: '600',
-          width: '70%',
-          textAlign: 'center',
-          'line-height': '1.333em'
-        }}
+        className={hide}
+        className='carousel_item'
+        // className='border'
       >
-        {subtitle}
+        <img id='slider' src={Icon} />
+        <p
+          className={``}
+          style={{
+            color: '#126BC5',
+            fontWeight: '600',
+            width: '70%',
+            textAlign: 'center',
+            'line-height': '1.333em'
+          }}
+        >
+          {subtitle}
+        </p>
       </p>
-    </p>
+    </div>
   </div>
 );
 export default ImageSlide;
