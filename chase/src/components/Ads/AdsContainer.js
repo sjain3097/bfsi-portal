6c52471d5be9317f416c70d30cd7cc2a26eebca2
sankalp <sjain4688@gmail.com>
2020-02-11 11:54:24 +0530

    added ads.css and moved styling to the externa css file

--- chase/src/components/Ads/AdsContainer.js
+++ chase/src/components/Ads/AdsContainer.js
@@ -3,59 +3,26 @@ import React, { Component } from 'react';
 export default class AdsContainer extends Component {
   render() {
     return (
-      <div
-        className=''
-        style={{
-          padding: ' 0 .5em 0 .5em',
-          marginLeft: '7%',
-          marginTop: '7%',
-          borderTop: '1px solid #d9d9d9',
-          //   backgroundColor: 'yellow',
-          width: '85%'
-        }}
-      >
-        <div style={{ marginTop: '3%', marginLeft: '2%' }}>
-          <div
-            className='ad_title '
-            style={{
-              display: 'inline-block',
-              width: '52%'
-            }}
-          >
-            <p
-              style={{
-                fontWeight: '300',
-                fontSize: '3.125em',
-                lineHeight: '56px',
-                color: '#414042'
-              }}
-            >
-              Get access to cash for your business
-            </p>
+      <div className='ad_tile'>
+        <div className='ad_inner_tile'>
+          <div className='ad_title_container '>
+            <p className='ad_title'>Get access to cash for your business</p>
           </div>
           <div
-            class=''
+            class='ad_img_container'
             style={{
-              float: 'left',
-              paddingRight: '24px',
-              paddingLeft: '0'
+              float: 'left'
             }}
           >
             <img
               src='https://sites.chase.com/content/services/structured-image/image.copy.jpg/XCRT/Tile-Secondary/2019/60438_19-289_CFBcampaign_SmTile.jpg'
-              alt='  '
+              alt=''
             />
           </div>
           <div class=''>
             <div class=''>
-              <div class=''>
-                <p
-                  style={{
-                    fontWeight: '300',
-                    fontSize: '18px',
-                    color: '#414042'
-                  }}
-                >
+              <div class='ad_content'>
+                <p>
                   Be ready for whatever comes your way with a Chase Business
                   Line of Credit.
                 </p>
