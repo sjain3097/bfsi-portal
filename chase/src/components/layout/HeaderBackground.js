21dbccb4ae1a8138407f992a5db78e31680d91ff
sankalp <sjain4688@gmail.com>
2020-02-19 15:59:01 +0530

    header background resposive properly working

--- chase/src/components/layout/HeaderBackground.js
+++ chase/src/components/layout/HeaderBackground.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import HeaderDropDown from './HeaderDropDown';
+import SignInCard from './SignInCard';
 export default class HeaderBackground extends Component {
   render() {
     return (
       <div>
-        <Header />
-        <HeaderDropDown />
+        <div className='header--background'>
+          <Header />
+          <HeaderDropDown />
+          <div style={{ position: 'relative', top: '80px' }}>
+            <div className='container'>
+              <div class='grid-container'>
+                <div class='item1'>
+                  <img src='200.png' />
+                </div>
+                <div class='item2'>
+                  <h1>New Chase customers:</h1>
+                </div>
+                <div class='item3'>
+                  <p>
+                    Open a Chase Total Checking® and Chase Savings℠ account with
+                    qualifying activities.
+                  </p>
+                </div>
+                <div class='item4'>
+                  <p className='col'>
+                    <a className='open_acc_button' href=''>
+                      Open an account{' '}
+                    </a>
+                  </p>
+                </div>
+
+                <div class='item5 d-m-block'>
+                  <SignInCard />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- chase/src/components/layout/SignInCard.js
+++ chase/src/components/layout/SignInCard.js
@@ -3,20 +3,17 @@ import React, { Component } from 'react';
 export default class SignInCard extends Component {
   render() {
     return (
-      <div className='card_sign_in d-s-block d-xs-block'>
-        <div className='card_sign_in--heading'>Welcome</div>
+      <div className='card_sign_in d-s-block d-xs-block '>
+        <div className='card_sign_in--heading '>Welcome</div>
         <div
           //className='card-body'
-          style={{
-            'margin-top': '5%',
-            'margin-left': '10%'
-          }}
+          style={{}}
         >
-          <a href='' className='sign_in_card_button'>
-            Sign in
+          <a href=''>
+            <p className='sign_in_card_button'>Sign in</p>
           </a>
         </div>
-        <div style={{ 'margin-top': '5%', 'margin-left': '10%' }}>
+        <div style={{ 'margin-left': '10%' }}>
           <a className='card_sign_in--link' href='#'>
             Not enrolled? Sign up now
           </a>
